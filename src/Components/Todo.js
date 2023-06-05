1633790bbe89b46f01a84d7b3b289270530ffafc
Unnimaya CK <unnimayack98@gmail.com>
2023-06-05 13:23:26 +0530

    Add feature delete of todos in delete button press

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,44 +1,72 @@
-import React from 'react';
-import './Todo.css';
-import { useState, useRef ,useEffect } from 'react';
-
+import React from "react";
+import "./Todo.css";
+import { useState, useRef, useEffect } from "react";
+import { MdDelete } from "react-icons/md";
+import { FiEdit } from "react-icons/fi";
+import { IoMdDoneAll } from "react-icons/io";
 function Todo() {
-  const [todo , setTodo] = useState('')
-  const [todos , setTodos] = useState([])
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
 
-  const handleSubmit = (e) =>{
+  const handleSubmit = (e) => {
     e.preventDefault();
-  } 
+  };
 
-  const addTodo =() =>{
-    setTodos([...todos,todo])
+  const addTodo = () => {
+    setTodos([...todos, {list : todo , id : Date.now()}]);
     console.log(todos);
-    setTodo('')
+    setTodo("");
+  };
+
+  const inputRef = useRef("null");
+
+  useEffect(() => {
+    inputRef.current.focus();
+  });
+
+  const onDelete = (id) =>{
+    setTodos(todos.filter((list) => list.id !== id))
   }
 
-  const inputRef = useRef('null');
-
-  useEffect(()=>{
-    inputRef.current.focus();
-  },)
-
   return (
-    <div className='container'>
+    <div className="container">
       <h2>TODO APP</h2>
-      <form onSubmit={handleSubmit} className='form-group'>
-        <input className='form-control' value={todo} type="text" ref={inputRef}   placeholder="Enter Your Todo" onChange={(event)=>setTodo(event.target.value)}/>
+      <form onSubmit={handleSubmit} className="form-group">
+        <input
+          className="form-control"
+          value={todo}
+          type="text"
+          ref={inputRef}
+          placeholder="Enter Your Todo"
+          onChange={(event) => setTodo(event.target.value)}
+        />
         <button onClick={addTodo}>ADD</button>
       </form>
-      <div className='list'>
+      <div className="list">
         <ul>
-         {todos.map((obj)=>
-            <li>{obj}</li>
-         )}
+          {todos.map((obj) => (
+            <li className="list-items">
+              <div className="list-item-list">{obj.list}</div>
+              <span>
+                <IoMdDoneAll
+                  className="list-item-icons"
+                  title="Complete"
+                  id="complete"
+                />
+                <FiEdit className="list-item-icons" title="Edit" id="edit" />
+                <MdDelete
+                  className="list-item-icons"
+                  title="Delete"
+                  id="delete"
+                  onClick={()=>onDelete(obj.id)}
+                />
+              </span>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
-  )
+  );
 }
 
-export default Todo
-
+export default Todo;
