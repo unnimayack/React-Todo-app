ab42cb1239be503e61b7526cc7a3916e6fa52237
Unnimaya CK <unnimayack98@gmail.com>
2023-06-05 08:49:58 +0530

    Add feature to focus on input field of todos

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Todo.css';
-import { useState } from 'react';
+import { useState, useRef ,useEffect } from 'react';
 
 function Todo() {
   const [todo , setTodo] = useState('')
@@ -13,12 +13,20 @@ function Todo() {
   const addTodo =() =>{
     setTodos([...todos,todo])
     console.log(todos);
+    setTodo('')
   }
+
+  const inputRef = useRef('null');
+
+  useEffect(()=>{
+    inputRef.current.focus();
+  },)
+
   return (
     <div className='container'>
       <h2>TODO APP</h2>
       <form onSubmit={handleSubmit} className='form-group'>
-        <input className='form-control' value={todo} type="text"   placeholder="Enter Your Todo" onChange={(event)=>setTodo(event.target.value)}/>
+        <input className='form-control' value={todo} type="text" ref={inputRef}   placeholder="Enter Your Todo" onChange={(event)=>setTodo(event.target.value)}/>
         <button onClick={addTodo}>ADD</button>
       </form>
       <div className='list'>
