528e0cfa8562dcf52b4f233068f0d3a3a229f08c
Unnimaya CK <unnimayack98@gmail.com>
2023-06-05 06:36:43 +0530

    Added todos are displayed as todolist

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,19 +1,31 @@
-import React from 'react'
-import './Todo.css'
-
+import React from 'react';
+import './Todo.css';
+import { useState } from 'react';
 
 function Todo() {
+  const [todo , setTodo] = useState('')
+  const [todos , setTodos] = useState([])
 
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+  } 
+
+  const addTodo =() =>{
+    setTodos([...todos,todo])
+    console.log(todos);
+  }
   return (
     <div className='container'>
       <h2>TODO APP</h2>
-      <form className='form-group'>
-        <input className='form-control' type="text"   placeholder="Enter Your Todo"  />
-        <button >ADD</button>
-      </form >
+      <form onSubmit={handleSubmit} className='form-group'>
+        <input className='form-control' value={todo} type="text"   placeholder="Enter Your Todo" onChange={(event)=>setTodo(event.target.value)}/>
+        <button onClick={addTodo}>ADD</button>
+      </form>
       <div className='list'>
         <ul>
-          <li>Read news paper</li>
+         {todos.map((obj)=>
+            <li>{obj}</li>
+         )}
         </ul>
       </div>
     </div>
